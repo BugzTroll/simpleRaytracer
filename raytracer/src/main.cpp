9d552a272640c697cbe7eb1d881efd82a0675d72
marie-Eve Dubé <mariedube2@gmail.com>
2020-04-01 14:33:16 -0400

    Adding simple opengl display of generated ppm image * change format of ppm image (using pnm P6 instead of ppm p3) to be supported stb_image * add stb_image header to load images * adding glfw and glad to cmake * add opengl functions to generate window with texture

--- raytracer/src/main.cpp
+++ raytracer/src/main.cpp
@@ -1,23 +1,46 @@
+#define STB_IMAGE_IMPLEMENTATION
+#define GLFW_INCLUDE_NONE
+#include "stb_image.h"
+
 #include <iostream>
 #include <fstream>
 #include <limits>
 #include <stdlib.h>
+#include <GLFW/glfw3.h>
+#include <math.h>
+
 #include "vec3.h"
 #include "vec3.cpp"
 #include "ray.h"
-#include <math.h>
 #include "hitableList.h"
 #include "sphere.h"
 #include "camera.h"
 
+#include <glad/glad.h>
+#include <vector>
+#include <algorithm> 
+
+
 using namespace std;
+
+vec3 randomInUnitSphere()
+{
+	vec3 p;
+	do
+	{
+		p = 2.0 * vec3(rand() / double(RAND_MAX), rand() / double(RAND_MAX), rand() / double(RAND_MAX)) - vec3(1,1,1);
+	} 
+	while (p.square_length() >= 1);
+	return p;
+}
 
 vec3 color(const ray& r, hitable* world)
 {
 	hitRecord rec;
-	if (world->hit(r, 0.0, numeric_limits<float>::max(), rec))
+	if (world->hit(r, 0.001, numeric_limits<float>::max(), rec))
 	{
-		return (0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1));
+		vec3 target = rec.p + rec.normal + randomInUnitSphere();
+		return (0.5 * color(ray(rec.p, target - rec.p), world));
 	}
 	else
 	{
@@ -26,12 +49,232 @@ vec3 color(const ray& r, hitable* world)
 		return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
 	}
 }
+
+// Shaders
+//const char* vertexShaderSource = "#version 330 core\n"
+//"layout (location = 0) in vec3 aPos;\n"
+//"void main()\n"
+//"{\n"
+//"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+//"}\0";
+
+//const char* fragmentShaderSource = "#version 330 core\n"
+//"out vec4 FragColor;\n"
+//"void main()\n"
+//"{\n"
+//"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+//"}\0";
+
+const char* vertexShaderSource =
+"#version 330 core\n"
+"layout(location = 1) in vec3 aColor;\n"
+"layout(location = 2) in vec2 aTexCoord;\n"
+"layout(location = 0) in vec3 aPos;\n"
+"out vec3 ourColor;\n"
+"out vec2 TexCoord;\n"
+"void main()\n"
+"{\n"
+"	gl_Position = vec4(aPos, 1.0);\n"
+"	ourColor = aColor;\n"
+"	TexCoord = aTexCoord;\n"
+"}\0";
+
+const char* fragmentShaderSource =
+"#version 330 core\n"
+"out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
+"in vec2 TexCoord;\n"
+"uniform sampler2D ourTexture;\n"
+"void main()\n"
+"{\n"
+"	FragColor = texture(ourTexture, TexCoord);\n"
+"}\0";
+
+
+unsigned int generateTexture(int shaderProgram)
+{
+	unsigned int texture;
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	// set the texture wrapping/filtering options (on the currently bound texture object)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
+	unsigned char* data = stbi_load("../outputImages/outputImage.pnm", &width, &height, &nrChannels, 0);
+
+	if (data)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+
+	glUseProgram(shaderProgram);
+
+	return texture;
+}
+
+int buildShaders()
+{
+	int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	// check for shader compile errors
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	// fragment shader
+	int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	// check for shader compile errors
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	// link shaders
+	int shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	// check for linking errors
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+	return shaderProgram;
+}
+
+unsigned int bindVerticeBuffers()
+{
+	float vertices[] = {
+		// positions          // colors           // texture coords
+		 1.0f,  1.0f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+		 1.0f, -1.0f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+		-1.0f, -1.0f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+		-1.0f,  1.0f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
+	};
+	unsigned int indices[] = {  // note that we start from 0!
+		0, 1, 3,  // first Triangle
+		1, 2, 3   // second Triangle
+	};
+	unsigned int VBO, VAO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	// texture coord attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	return VAO;
+}
+
+int createWindow()
+{
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(800, 400, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// set gl viewport
+	glViewport(0, 0, 800, 400);
+
+	// build shaders
+	int shaderProgram = buildShaders();
+
+	// bind buffers
+	unsigned int VAO = bindVerticeBuffers();
+
+	// load and bind texture
+	unsigned int texture = generateTexture(shaderProgram);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture);
+
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
 int main()
 {
-	cout << "Writing to file" << endl;
+	std::cout << "Writing to file" << endl;
 
 	// open image file
-	ofstream myfile("../outputImages/outputImage.ppm");
+	ofstream myfile("../outputImages/outputImage.pnm", std::ios::binary);
 
 	int nx = 200;
 	int ny = 100;
@@ -43,9 +286,10 @@ int main()
 	list[0] = new sphere(vec3(0, 0, -1), 0.5);
 	list[1] = new sphere(vec3(0, -100.5, -1), 100);
 	hitable* world = new hitableList(list, 2);
-	myfile << "P3\n" << nx << " " << ny << "\n255\n";
+	myfile << "P6\n" << nx << " " << ny << "\n255\n";
 
 	// Send rays for every pixels
+	int cpt = 0;
 	for (int j = ny - 1; j >= 0; j--) {
 		for (int i = 0; i < nx; i++) {
 
@@ -63,16 +307,22 @@ int main()
 
 			col /= float(ns);
 
-			int ir = int(255.99 * col.r());
-			int ig = int(255.99 * col.g());
-			int ib = int(255.99 * col.b());
-			myfile << ir << " " << ig << " " << ib << "\n";
+			//gamma correction
+			col = vec3(sqrt(col.r()), sqrt(col.g()), sqrt(col.b()));
+
+			unsigned char r = static_cast<unsigned char>(std::min(1.f, col.r()) * 255);
+			unsigned char g = static_cast<unsigned char>(std::min(1.f, col.g()) * 255);
+			unsigned char b = static_cast<unsigned char>(std::min(1.f, col.b()) * 255);
+			myfile << r << g << b;
 		}
 	}
+
 	myfile.close();
 
-	cout << "done" << endl;
+	createWindow();
 
-	cin >> nx;
+	std::cout << "done" << endl;
+
+	std::cin >> nx;
 	return 0;
 }
